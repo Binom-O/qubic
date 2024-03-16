a3e44dae99ff0bdecc2a607ad36a169400882536
Binom-O <159266722+Binom-O@users.noreply.github.com>
2024-03-16 03:32:07 +0300

    Update Bin_qubicli_1.8.8_cpu_gpu.sh

--- Bin_qubicli_1.8.8_cpu_gpu.sh
+++ Bin_qubicli_1.8.8_cpu_gpu.sh
@@ -13,7 +13,6 @@ source /hive-config/rig.conf
 cat <<EOF > appsettings.json
 {
   "Settings": {
-   "useAvx2" : "false",
     "overwrites": {"CUDA": "12"},
     "allowHwInfoCollect": true,
     "baseUrl": "https://mine.qubic.li/",
@@ -35,12 +34,9 @@ cd /root/qubic_cpu
 cat <<EOF > appsettings.json
 {
   "Settings": {
-   "useAvx2" : "false",
-    "overwrites": {"CUDA": "12"},
-    "allowHwInfoCollect": true,
+    "overwrites": {},
     "baseUrl": "https://mine.qubic.li/",
     "amountOfThreads": $(nproc),
-    "payoutId": null,
     "accessToken":"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImQ0MGMwOWE0LWQzYTUtNDc4Ni05YzRhLTJjNGM2OTYxZWMxMCIsIk1pbmluZyI6IiIsIm5iZiI6MTcxMDMzNzYzMCwiZXhwIjoxNzQxODczNjMwLCJpYXQiOjE3MTAzMzc2MzAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.M0LReOH6-tu_fxo22ctvccgm_ZMWSkBv_ydgDtVUNm6ljfkfpgYjEV5s_T0MitzzOM145WrXlSFouC8EFF1DBA",
     "alias": "$WORKER_NAME"
   }
