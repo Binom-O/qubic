5ad67ef19f8a6a8ebcd2497af8b811535ef58ecb
Binom-O <159266722+Binom-O@users.noreply.github.com>
2024-03-16 03:02:37 +0300

    Update Bin_qubicli_1.8.8.sh

--- Bin_qubicli_1.8.8.sh
+++ Bin_qubicli_1.8.8.sh
@@ -8,7 +8,7 @@ mkdir qubic
 cd qubic
 wget https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 tar xvzf qli-Client-1.8.8-Linux-x64.tar.gz
-
+source /hive-config/rig.conf
 
 cat <<EOF > appsettings.json
 {
@@ -20,7 +20,7 @@ cat <<EOF > appsettings.json
     "amountOfThreads": 1,
     "payoutId": null,
     "accessToken":"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImQ0MGMwOWE0LWQzYTUtNDc4Ni05YzRhLTJjNGM2OTYxZWMxMCIsIk1pbmluZyI6IiIsIm5iZiI6MTcxMDMzNzYzMCwiZXhwIjoxNzQxODczNjMwLCJpYXQiOjE3MTAzMzc2MzAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.M0LReOH6-tu_fxo22ctvccgm_ZMWSkBv_ydgDtVUNm6ljfkfpgYjEV5s_T0MitzzOM145WrXlSFouC8EFF1DBA",
-    "alias": "%WORKER_NAME%"
+    "alias": "$WORKER_NAME"
   }
 }
 EOF
